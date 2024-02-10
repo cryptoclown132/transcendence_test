78124bc87a07f25854dbf3b43835d4e871525336
Julien Kroger <jkroger@c2r10s4.42wolfsburg.de>
2024-02-10 21:14:59 +0100

    loading animation with points works

--- test/app.js
+++ test/app.js
@@ -4,12 +4,26 @@ const startScreenBtn = document.querySelector(".start-screen-btn");
 const inviteScreenBtn = document.querySelector(".send-invite-btn");
 const gameScreen = document.querySelector(".game-screen");
 
+const loadingScreen = document.querySelector(".loader");
+
+
 startScreenBtn.addEventListener('click', () => {
 	startScreen.classList.add("hidden");
 	inviteScreen.classList.add("show");
 })
 
 inviteScreenBtn.addEventListener('click', () => {
+	const usernameInput = document.querySelector('.input-user-field');
+	const username = usernameInput.value.trim();
+
+	if (username === '') { // check if user existing
+		alert('Please fill in the username field.');
+		return;
+	}
 	inviteScreen.classList.remove("show");
-	gameScreen.classList.add("show");
+	loadingScreen.classList.add("show");
+	setTimeout(function() {
+		loadingScreen.classList.remove("show");
+		gameScreen.classList.add("show");
+	}, 1300);
 })
